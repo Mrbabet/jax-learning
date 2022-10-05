c5cc05239931effe4a01b6ffd5db93e715746b2c
Mrbabet <wieliczkokamil@gmail.com>
2022-10-06 00:06:49 +0200

    working o price slider

--- script.js
+++ script.js
@@ -30,3 +30,46 @@ sortBtn.addEventListener("click", () => {
     sortOpen = false;
   }
 });
+
+const rangeInput = document.querySelectorAll(".input-range-slider input"),
+  priceInput = document.querySelectorAll(".input-range-input input"),
+  progress = document.querySelector(".slider .progress");
+
+let priceGap = 1000;
+
+priceInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(priceInput[0].value),
+      maxVal = parseInt(priceInput[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "input-min") {
+        rangeInput[0].value = minVal;
+        progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      } else {
+        rangeInput[1].value = maxVal;
+        progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+      }
+    }
+  });
+});
+
+rangeInput.forEach((input) => {
+  input.addEventListener("input", (e) => {
+    let minVal = parseInt(rangeInput[0].value),
+      maxVal = parseInt(rangeInput[1].value);
+
+    if (maxVal - minVal < priceGap) {
+      if (e.target.className === "range-min") {
+        rangeInput[0].value = maxVal - priceGap;
+      } else {
+        rangeInput[1].value = minVal + priceGap;
+      }
+    } else {
+      rangeInput[0].value = minVal;
+      rangeInput[1].value = maxVal;
+      progress.style.left = (minVal / rangeInput[0].max) * 100 + "%";
+      progress.style.right = 100 - (maxVal / rangeInput[1].max) * 100 + "%";
+    }
+  });
+});
