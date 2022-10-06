4f8911c68340b447be2fa70a72f9b47422cf34d4
Mrbabet <wieliczkokamil@gmail.com>
2022-10-06 19:52:59 +0200

    Added button which changes view from grid to list

--- script.js
+++ script.js
@@ -72,3 +72,21 @@ rangeInput.forEach((input) => {
     }
   });
 });
+
+var wrapper = document.getElementById("products-container");
+
+document.addEventListener("click", function (event) {
+  if (!event.target.matches(".list")) return;
+
+  // List view
+  event.preventDefault();
+  wrapper.classList.add("list");
+});
+
+document.addEventListener("click", function (event) {
+  if (!event.target.matches(".grid")) return;
+
+  // List view
+  event.preventDefault();
+  wrapper.classList.remove("list");
+});
